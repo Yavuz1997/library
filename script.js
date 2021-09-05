81dcccbcb431e691f113075109382d522a99a1a6
Yavuz <yavuzgogus@gmail.com>
2021-09-05 20:07:16 +0300

    test code

---
+++ script.js
@@ -0,0 +1,8 @@
+let books = [];
+
+function Book(title, author, numOfPages, readStatus){
+    this.title = title;
+    this.author = author;
+    this.numOfPages = numOfPages;
+    this.readStatus = readStatus;
+}
