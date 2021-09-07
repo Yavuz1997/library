7f948ae69248026b6c1234881dae4475d0e0f9e3
Yavuz <yavuzgogus@gmail.com>
2021-09-07 19:57:37 +0300

    buttons now open/close form window

--- script.js
+++ script.js
@@ -9,16 +9,24 @@ function Book(title, author, numOfPages, readStatus) {
 Book.prototype.info = function () {
     console.log("Title: " + this.title + " Author: " + this.author + " Pages: " + this.numOfPages + " Status: " + this.readStatus);
 };
-const addBtn = document.querySelector("#submit");
+const submit = document.querySelector("#submit");
 const title = document.querySelector("#title");
 const author = document.querySelector("#author");
 const pages = document.querySelector("#numOfPages");
 const readingStatus = document.querySelector("#readingStatus");
+const addBtn = document.querySelector("#addButton");
+const formWindow = document.querySelector("#formWrapper");
 
-addBtn.addEventListener("click", () => {
+addBtn.addEventListener("click",() => {
+    console.log("hey");
+    formWindow.style.display = "flex";
+});
+
+submit.addEventListener("click", () => {
     let book = new Book(title.value, author.value, pages.value, readingStatus.value);
     books.push(book);
     for (let i = 0; i < books.length; i++) {
         books[i].info();
     }
+    formWindow.style.display = "none";
 });
