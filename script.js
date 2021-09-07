892b0ba77397079ad245b99bdaa9396500ca8173
Yavuz <yavuzgogus@gmail.com>
2021-09-07 16:49:38 +0300

    Changed form place, now it opens a window ( not functional yet)

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 let books = [];
 
-function Book(title, author, numOfPages, readStatus){
+function Book(title, author, numOfPages, readStatus) {
     this.title = title;
     this.author = author;
     this.numOfPages = numOfPages;
     this.readStatus = readStatus;
 }
+Book.prototype.info = function () {
+    console.log("Title: " + this.title + " Author: " + this.author + " Pages: " + this.numOfPages + " Status: " + this.readStatus);
+};
+const addBtn = document.querySelector("#submit");
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#numOfPages");
+const readingStatus = document.querySelector("#readingStatus");
+
+addBtn.addEventListener("click", () => {
+    let book = new Book(title.value, author.value, pages.value, readingStatus.value);
+    books.push(book);
+    for (let i = 0; i < books.length; i++) {
+        books[i].info();
+    }
+});
